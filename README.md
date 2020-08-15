0e9cd2cf9f031dde945aceb276552a4c1ca58d10
narlylily <narlylily@gmail.com>
2020-08-15 11:23:51 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# obs_art_json_maker
+a very very simple web app for generating a json from selected files!
+made to be used w/ https://github.com/adrianmgg/obs_art_slideshow
+it looks ugly lol
+
+# instructions
+1. download on your computer
+2. copy path to index.html
+3. paste path in browser of choice! not internet explorer tho pls
+4. use the browse button to select files to add to the json object
+5. press the display json button to print out the json object on screen so you can copy it!
---
+++ app.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var json = [];
+    function readMultipleFiles(evt) {
+        //Retrieve all the files from the FileList object
+        var files = evt.target.files; 
+        
+        if (files) {
+            for (var i=0, f; f=files[i]; i++) {
+                var r = new FileReader();
+                r.onload = (function(f) {
+                    return function(e) {
+                        if(f.type.includes("image")) {
+                            json.push({
+                            "path" : "images/" + f.name,
+                            "artist" : "tbd",
+                            "type" : "image"
+                            });
+                        } else if (f.type.includes("video") || f.type.includes("application/x-mpegUR")) {
+                            json.push({
+                            "path" : "images/" + f.name,
+                            "artist" : "tbd",
+                            "type" : "video"
+                            });
+                        } else {
+                            console.log(f.name + ' did not load.');
+                        }
+                    };
+                })(f);
+                r.readAsText(f);
+            }   
+        } else {
+            alert("Failed to load files"); 
+        }
+    }
+
+    function displayJSON() {
+        document.getElementById('response').innerHTML = JSON.stringify(json);
+    }
+
+    document.getElementById('fileinput').addEventListener('change', readMultipleFiles, false);
+    document.getElementById('display').addEventListener('click', displayJSON, false);
+})
